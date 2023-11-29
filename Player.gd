9940a5be88cac7c5e600ff5fe36b7f92fbca905f
kyeshmz <kyeshimizu@gmail.com>
2023-11-30 07:53:51 +0900

    add update to player movement

--- Player.gd
+++ Player.gd
@@ -1,5 +1,6 @@
-extends Area2D
+extends CharacterBody2D
 signal hit
+
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
@@ -12,7 +13,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://www.youtube.com/watch?v=S8lMTwSRoRg
+https://www.youtube.com/watch?v=wX145eoLFSM&list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a&index=5
